fa8d0b56657e3ab32d4062ed32f27be87b9cb367
Sriram <46366477+ram170@users.noreply.github.com>
2020-06-07 02:21:59 +0530

    Updates

---
+++ precache-manifest.9767d09c3ec32d351cf395b1f5674ab3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52420fdb45ee9df0a8e5e9e3bc32df22",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "af11f3e04ce8e6af5020",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c25d8f1f8950cf6c4492",
+    "url": "/portfolio/static/css/main.40f2e1f2.chunk.css"
+  },
+  {
+    "revision": "af11f3e04ce8e6af5020",
+    "url": "/portfolio/static/js/2.fd60dc4c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio/static/js/2.fd60dc4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25d8f1f8950cf6c4492",
+    "url": "/portfolio/static/js/main.9086966e.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "cb9d73ae790d25c2633cf71af9f9f8de",
+    "url": "/portfolio/static/media/myImage.cb9d73ae.jpg"
+  }
+]);
